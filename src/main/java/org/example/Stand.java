7c3bb2bbc843ec51ab05a20c7622bee9ab2c615d
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-06 22:16:03 +0100

    Habe mit Vasilli <3 lombock installiert danke dir

--- src/main/java/org/example/Stand.java
+++ src/main/java/org/example/Stand.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "stands")
 public class Stand {
+    // Getter und Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -12,36 +17,4 @@ public class Stand {
     private String ability;
     private int range;
 
-    // Getter und Setter
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getStandname() {
-        return standname;
-    }
-
-    public void setStandname(String standname) {
-        this.standname = standname;
-    }
-
-    public String getAbility() {
-        return ability;
-    }
-
-    public void setAbility(String ability) {
-        this.ability = ability;
-    }
-
-    public int getRange() {
-        return range;
-    }
-
-    public void setRange(int range) {
-        this.range = range;
-    }
 }
