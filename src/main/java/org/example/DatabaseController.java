5791f97de43504189ded397da08afb02e13bc646
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-11 17:59:24 +0100

    Eigentlich nur paar Sachen ausprobiert klappt aber net

--- src/main/java/org/example/DatabaseController.java
+++ src/main/java/org/example/DatabaseController.java
@@ -3,9 +3,11 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +23,13 @@ import java.util.List;
         public List<Stand> getAllStands(String name) {
             return entityManager.createQuery("SELECT s FROM Stand s WHERE s.standname LIKE :standname", Stand.class).setParameter("standname",name).getResultList();
         }
+
+        @PostMapping
+        public ResponseEntity<Stand> createStand(@RequestBody Stand stand) {
+            entityManager.persist(stand);
+            return ResponseEntity.status(HttpStatus.CREATED).body(stand);
+        }
+
+
     }
 
