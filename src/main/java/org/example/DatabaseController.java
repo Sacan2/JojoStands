40375a625e27d050160f68f6032f6f9f1e5ae7e5
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-16 22:01:26 +0100

    Den Get Request eingefügt

--- src/main/java/org/example/DatabaseController.java
+++ src/main/java/org/example/DatabaseController.java
@@ -5,9 +5,10 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
-    @RestController
+@RestController
     @RequestMapping("/api/stands")
     public class DatabaseController {
 
@@ -15,7 +16,11 @@ import org.springframework.web.bind.annotation.*;
 
         @PersistenceContext
         private EntityManager entityManager;
-
+        @GetMapping
+        public List<Stand> getAllStands(String name) {
+            return entityManager.createQuery("SELECT s FROM Stand s WHERE s.standname LIKE :standname",
+                    Stand.class).setParameter("standname","%" + name + "%").getResultList();
+        }
         @Autowired
         private StandRepository standRepository;
         @PostMapping("/addStand")
