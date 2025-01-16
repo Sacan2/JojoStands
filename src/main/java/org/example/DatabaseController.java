bd9d5219cfbd5505bfb9ab01a85d8c7e68334e62
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-16 21:57:45 +0100

    Es lag am Auto generate, es wurde die ganze Zeit eine neue ID erstellt während ich eine ID neu gesetzt habe dabei wurde nichts überschrieben, sondern entweder zusammen gesetzt oder es wurde tatsächlich immer überschrieben.

--- src/main/java/org/example/DatabaseController.java
+++ src/main/java/org/example/DatabaseController.java
@@ -1,31 +1,21 @@
 package org.example;
 
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 
     @RestController
     @RequestMapping("/api/stands")
     public class DatabaseController {
 
-        @PersistenceContext
-        private EntityManager entityManager;
-        // GET: Alle Stands abrufen
-        @GetMapping
-        public List<Stand> getAllStands(String name) {
-            return entityManager.createQuery("SELECT s FROM Stand s WHERE s.standname LIKE :standname",
-                    Stand.class).setParameter("standname","%" + name + "%").getResultList();
-        }
 
         @Autowired
         private StandRepository standRepository;
-
-
+        @PostMapping("/addStand")
+        public void addStand(@RequestBody Stand stand){
+            standRepository.save(stand);
+        }
 
 
     }
--- src/main/java/org/example/Stand.java
+++ src/main/java/org/example/Stand.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 public class Stand {
     // Getter und Setter
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "standname")
